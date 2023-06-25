5c1cf43873323c2d79bf23603bb0327baa100d6a
Bryan Amds <bryan.amadeus1003@gmail.com>
2023-06-25 12:22:01 +0700

    Updated Premium but still can't update "isPremium" status pls elp

--- AoL_PSD/Views/Premium.aspx.cs
+++ AoL_PSD/Views/Premium.aspx.cs
@@ -4,14 +4,33 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using AoL_PSD.Handler;
+using AoL_PSD.Model;
 
 namespace AoL_PSD.Views
 {
     public partial class Premium : System.Web.UI.Page
     {
+        UserHandler uh = new UserHandler();
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                User currentUser = uh.GetCurrentUser();
+                Session["CurrentUser"] = currentUser; // Store the current user in a session variable for easy access
+            }
+        }
 
+        protected void submitBtn_Click(object sender, EventArgs e)
+        {
+            User currentUser = uh.GetCurrentUser();
+            if (currentUser != null)
+            {
+                uh.updatePremium(currentUser);
+            }
+
+
+            Response.Redirect("~/Views/Home.aspx");
         }
     }
 }
